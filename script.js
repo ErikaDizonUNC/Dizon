8474fd06b7e4c5a679df36d196e9e2ea5e2eeb72
Dizon Erika Mae P. <erikamae.dizon@unc.edu.ph>
2023-11-11 13:53:42 +0800

    updated

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-const registrationForm = document.getElementById('registrationForm');
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-registrationForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-
-  
-const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  // Send registration data to server using AJAX
-  fetch('/register.php', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name,
-      email,
-      password,
-    }),
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.success) {
-      // Registration successful, show verification message
-      registrationForm.style.display = 'none';
-      const verificationContainer = document.querySelector('.container.verification');
-      verificationContainer.style.display = 'block';
-    } else {
-      // Registration error, display error message
-      alert(data.error);
-    }
-  });
-});
+  // Check credentials (this is a simple example, not secure for real-world use)
+  if (username === "user" && password === "pass") {
+      alert("Login successful!");
+  } else {
+      alert("Invalid credentials. Please try again.");
+  }
+}
